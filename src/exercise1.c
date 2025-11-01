2b3545c6138f9c6c8d9e7118ee1ce703c415d3e3
OHANA23913 <aprvapra23@mail.ru>
2025-11-01 21:02:09 +0700

    Fix compilation issues and file paths

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    double numbers[5];
+    double sum = 0.0;
+    
+    printf("Enter 5 numbers:\n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%lf", &numbers[i]);
+        sum += numbers[i];
+    }
+    
+    double average = sum / 5;
+    printf("Average: %.2f\n", average);
+
     return 0;
 }
